05591b00e4ec87337a5b12d8d8a7621ae0dbedb2
yutojubako <ytim8812@keio.jp>
2023-07-11 18:05:32 +0900

    [Change] use roboflow api model

--- detect.py
+++ detect.py
@@ -8,23 +8,15 @@ def predict(img_path):
     rf = Roboflow(api_key="SvoHMJJPTP96xfhtZcMk")
     project = rf.workspace().project("aicook-lcv4d")
     model = project.version(2).model
-    # model = YOLO('./weight/best.pt')  # pretrained YOLOv8n model
 
     out_dir = "."
     out_name = "predict"
-    # model.predict(img_path,project=out_dir,name=out_name,save=True, conf=0.5,save_txt=True)
-    detection_result = model.predict(img_path, confidence=40, overlap=30)
+    detection_result = model.predict(img_path, confidence=40, overlap=30).json()
 
     # Extract the 'class' from each prediction
     detected_classes = [prediction['class'] for prediction in detection_result['predictions']]
-
-    filename_with_ext = os.path.basename(img_path)
-    filename, _ = os.path.splitext(filename_with_ext)
-
-    labels = get_label(os.path.join(out_dir,out_name,"labels",f"{filename}.txt"))
-    #directoryをコピー
-    shutil.copytree(os.path.join(out_dir,out_name),os.path.join(out_dir,"output",str(datetime.datetime.now().strftime('%Y%m%d%H%M%S'))))
-    shutil.rmtree(os.path.join(out_dir,out_name))
+    # Remove duplicate classes
+    labels = list(set(detected_classes))
     return labels
 
 if __name__ == "__main__":
